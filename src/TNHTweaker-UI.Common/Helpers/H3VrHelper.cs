2bfbf1f8a25df13f98b481ebf94a40160f4b8849
DonderendeGenie <72990788+DonderendeGenie@users.noreply.github.com>
2020-10-21 22:30:44 +0200

    Added implementation for retrieving games install path through windows registry

--- src/TNHTweaker-UI.Common/Helpers/H3VrHelper.cs
+++ src/TNHTweaker-UI.Common/Helpers/H3VrHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.IO;
 using TNHTweaker_UI.Common.Interfaces;
 
@@ -9,17 +10,25 @@ namespace TNHTweaker_UI.Common.Helpers
     /// </summary>
     public class H3VrHelper : IH3VrHelper
     {
+        private const string h3vrRegistryPath = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 450540";
+
         public DirectoryInfo FindH3VrDirectory()
         {
-            throw new NotImplementedException();
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(h3vrRegistryPath);
+            if (key != null)
+            {
+                return new DirectoryInfo(key.GetValue("InstallLocation").ToString());
+            }
+            return null;
         }
 
         public bool IsTakeAndHoldTweakerInstalled()
         {
             var h3VrDirectory = FindH3VrDirectory();
             if (h3VrDirectory == null)
+            {
                 return false;
-
+            }
             return true;
         }
     }
--- src/TNHTweaker-UI.WPF/MainWindow.xaml.cs
+++ src/TNHTweaker-UI.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TNHTweaker_UI.Common;
+using TNHTweaker_UI.Common.Helpers;
 
 namespace TNHTweaker_UI.WPF
 {
