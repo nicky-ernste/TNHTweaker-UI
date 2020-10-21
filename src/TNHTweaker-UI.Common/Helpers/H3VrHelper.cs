3f408faec61c8e02f4436de8b828d199f2ab0099
Nicky Ernste <nicky.ernste@cgi.com>
2020-10-21 22:06:34 +0200

    Starting on H3VR detection

---
+++ src/TNHTweaker-UI.Common/Helpers/H3VrHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using TNHTweaker_UI.Common.Interfaces;
+
+namespace TNHTweaker_UI.Common.Helpers
+{
+    /// <summary>
+    /// Implementation of the <see cref="IH3VrHelper"/> interface.
+    /// </summary>
+    public class H3VrHelper : IH3VrHelper
+    {
+        public DirectoryInfo FindH3VrDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsTakeAndHoldTweakerInstalled()
+        {
+            var h3VrDirectory = FindH3VrDirectory();
+            if (h3VrDirectory == null)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ src/TNHTweaker-UI.Common/Interfaces/IH3VrHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace TNHTweaker_UI.Common.Interfaces
+{
+    /// <summary>
+    /// Interface that describes helper functions for working with the H3VR game and directories.
+    /// </summary>
+    public interface IH3VrHelper
+    {
+        /// <summary>
+        /// Attempt to find the directory on the local system where H3VR is installed.
+        /// </summary>
+        /// <returns>A <see cref="DirectoryInfo"/> object with information about the H3VR location or <c>null</c> if the H3VR installation was not found.</returns>
+        DirectoryInfo FindH3VrDirectory();
+
+        /// <summary>
+        /// Checks if the take and hold tweaker plugin for BepInEx is installed on this system.
+        /// Can only return <c>true</c> if <see cref="FindH3VrDirectory"/> returns a valid path to check.
+        /// </summary>
+        /// <returns><C>true</C> if the Take and Hold Tweaker plugin in present, <c>false</c> otherwise.</returns>
+        bool IsTakeAndHoldTweakerInstalled();
+    }
+}
