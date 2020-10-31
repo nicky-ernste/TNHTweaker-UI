0c1c3ca3ad22c1b5651f88c09f8f0fecf9a0f9fb
Nicky Ernste <nicky.ernste@cgi.com>
2020-10-31 14:11:23 +0100

    Created base character converter UI with some styles and MVVM.

---
+++ src/TNHTweaker-UI.CustomCharacterConverter/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using TNHTweaker_UI.BusinessLogic;
+using TNHTweaker_UI.BusinessLogic.Interfaces;
+using TNHTweaker_UI.CustomCharacterConverter.Views;
+
+namespace TNHTweaker_UI.CustomCharacterConverter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        ///<inheritdoc cref="PrismApplicationBase.RegisterTypes"/>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<ICharacterConverter, CharacterConverter>();
+            containerRegistry.RegisterSingleton<ICharacterSerializerOld, CharacterSerializerOld>();
+            containerRegistry.RegisterSingleton<ICharacterSerializer, CharacterSerializer>();
+        }
+
+        ///<inheritdoc cref="PrismApplicationBase.CreateShell"/>
+        protected override Window CreateShell() => Container.Resolve<MainWindow>();
+    }
+}
---
+++ src/TNHTweaker-UI.CustomCharacterConverter/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using PropertyChanged;
+using TNHTweaker_UI.BusinessLogic.Interfaces;
+using TNHTweaker_UI.CustomCharacterConverter.Views;
+
+namespace TNHTweaker_UI.CustomCharacterConverter.ViewModels
+{
+    /// <summary>
+    /// View model for the <see cref="MainWindow"/> view.
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel : BindableBase
+    {
+        private readonly ICharacterSerializerOld _oldCharacterSerializer;
+        private readonly ICharacterSerializer _newCharacterSerializer;
+        private readonly ICharacterConverter _characterConverter;
+
+        public string ApplicationTitle { get; set; } = "Take and Hold Custom character converter";
+
+        public MainWindowViewModel(ICharacterSerializerOld oldCharacterSerializer, ICharacterSerializer newCharacterSerializer, ICharacterConverter characterConverter)
+        {
+            _oldCharacterSerializer = oldCharacterSerializer;
+            _newCharacterSerializer = newCharacterSerializer;
+            _characterConverter = characterConverter;
+        }
+    }
+}
